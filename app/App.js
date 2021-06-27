3dadf69ae0f275d7fc5b6058e09bc933e63a3c0c
Kanishq Sunil <kanishq@Kanishq-Sunil.local>
2021-06-27 18:14:16 +0530

    Global Keyboard Test - completed

--- app/App.js
+++ app/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 export default function App() {
 
+
+    const setPreference = () => {
+
+    }
+
     return (
         <div>
-            Sample React Application
+            <input defaultValue="Search repos" />
+            <section>
+                <button onClick={setPreference} >Set Preference</button>
+                <p>Default option: Command + G</p>
+            </section>
+            <section>
+                <h6>
+                    Press Command + Q to quit applicaiton. 
+                </h6>
+            </section>
         </div>
     );
 }
--- main.js
+++ main.js
@@ -1,32 +1,76 @@
-const { BrowserWindow, app, ipcMain } = require('electron');
+const { BrowserWindow, app, ipcMain, Tray, Menu, MenuItem, globalShortcut } = require('electron');
 const path = require('path');
 
-// const isDev = !app.isPackaged;
+let mainWindow;
+let tray = "nothing";
 
-let win = null;
+app.on('ready', ()=> {
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 400,
+  mainWindow = new BrowserWindow({
     height: 400,
+    width: 300,
+    frame: false,
     show: false,
-    menuBarVisible: false,
+    resizable: false,
     webPreferences: {
-      nodeIntegration: false,
-      worldSafeExecuteJavaScript: true,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname,'preload.js')
     }
-  })
-
-  // win.setMenu(null);
-
-  win.loadFile('./app/index.html');
-
-  win.once('ready-to-show', ()=> {
-      win.show();
   });
 
-}
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 
-app.whenReady().then(createWindow)
+  const iconName = process.platform === 'win32' ? 'icon.ico' : 'icon-128.png';
+  const iconPath = path.join(__dirname,`/assets/${iconName}`);
+
+  tray = new Tray(iconPath);
+  tray.setToolTip("Cruize");
+
+  const menu = new Menu();
+
+  // Setting up Browser Shortcuts.
+
+  menu.append(new MenuItem({
+    label: 'Electron',
+    submenu: [{
+      role: 'help',
+      accelerator: process.platform === 'darwin' ? 'Cmd+Q' : 'Ctrl+Q',
+      click: () => { 
+          console.log("Quitting Electron Tray Application...");
+          app.quit();
+       }
+    }]
+  }));
+
+  Menu.setApplicationMenu(menu);
+
+  // Setting up Global Shortcuts
+
+  globalShortcut.register('CommandOrControl+I', () => {
+    console.log('Electron loves global shortcuts!');
+  });
+
+  tray.on('click', (events, bound) => {
+
+    const {x,y} = bound;
+    const { height, width } = mainWindow.getBounds();
+
+    // Toggling Visibility
+
+    if(mainWindow.isVisible()){
+      mainWindow.hide();
+    } else {
+
+      const yPos = process.platform === 'darwin' ? y : (y - height);
+      const xPos = x - parseInt(width / 2);
+
+      mainWindow.setBounds({
+        x: xPos, y: yPos, width: 300, height: 400
+      });
+
+      mainWindow.show();
+
+    }
+
+  })
+
+})
